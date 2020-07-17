cbb3032f640c783dbc63042ef177ae174917a1c8
GranThed <benjamincanas1@gmail.com>
2020-07-17 20:05:43 -0300

    Updates

---
+++ precache-manifest.b122cb6337a4105285c031f49bf4912d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf11e3baa41df8301e6a82b3611c3b61",
+    "url": "/grandthed.github.io/drum-machine/index.html"
+  },
+  {
+    "revision": "758cd581f6f94797101f",
+    "url": "/grandthed.github.io/drum-machine/static/css/main.c2d7dfea.chunk.css"
+  },
+  {
+    "revision": "a7bb27e65e3273bdaf31",
+    "url": "/grandthed.github.io/drum-machine/static/js/2.d258ba98.chunk.js"
+  },
+  {
+    "revision": "51459ba9cea87b6a576e8e949a89eb86",
+    "url": "/grandthed.github.io/drum-machine/static/js/2.d258ba98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "758cd581f6f94797101f",
+    "url": "/grandthed.github.io/drum-machine/static/js/main.2207dd19.chunk.js"
+  },
+  {
+    "revision": "10228a3221128d341254",
+    "url": "/grandthed.github.io/drum-machine/static/js/runtime-main.83529550.js"
+  }
+]);
